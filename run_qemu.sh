eac509c76a85f9dd98fa74121caf2c620ec99edc
WonTeu <gabriel.szabo2006@gmail.com>
2025-08-17 00:47:36 +0000

    organization change

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,1 +1,1 @@
-sudo qemu-system-x86_64 -cdrom dist/x86_64/kernel.iso -device isa-debugcon,chardev=dbg_console -chardev stdio,id=dbg_console
+sudo qemu-system-x86_64 -cdrom build/kernel.iso -device isa-debugcon,chardev=dbg_console -chardev stdio,id=dbg_console
