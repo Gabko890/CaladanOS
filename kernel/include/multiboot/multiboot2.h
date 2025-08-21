4646f0bd75c8f1f8f164ee4996de9d9c094c38ea
WonTeu <gabriel.szabo2006@gmail.com>
2025-08-21 20:14:13 +0000

    multiboot2 info parsing added

---
+++ kernel/include/multiboot/multiboot2.h
@@ -0,0 +1,79 @@
+#ifndef MULTIBOOT2_H
+#define MULTIBOOT2_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#define MULTIBOOT2_MAGIC 0x36d76289
+
+// Multiboot2 tag types
+#define MULTIBOOT_TAG_TYPE_END              0
+#define MULTIBOOT_TAG_TYPE_CMDLINE          1
+#define MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME 2
+#define MULTIBOOT_TAG_TYPE_MODULE           3
+#define MULTIBOOT_TAG_TYPE_BASIC_MEMINFO    4
+#define MULTIBOOT_TAG_TYPE_BOOTDEV          5
+#define MULTIBOOT_TAG_TYPE_MMAP             6
+#define MULTIBOOT_TAG_TYPE_VBE              7
+#define MULTIBOOT_TAG_TYPE_FRAMEBUFFER      8
+#define MULTIBOOT_TAG_TYPE_ELF_SECTIONS     9
+#define MULTIBOOT_TAG_TYPE_APM              10
+#define MULTIBOOT_TAG_TYPE_EFI32            11
+#define MULTIBOOT_TAG_TYPE_EFI64            12
+#define MULTIBOOT_TAG_TYPE_SMBIOS           13
+#define MULTIBOOT_TAG_TYPE_ACPI_OLD         14
+#define MULTIBOOT_TAG_TYPE_ACPI_NEW         15
+#define MULTIBOOT_TAG_TYPE_NETWORK          16
+#define MULTIBOOT_TAG_TYPE_EFI_MMAP         17
+#define MULTIBOOT_TAG_TYPE_EFI_BS           18
+#define MULTIBOOT_TAG_TYPE_EFI32_IH         19
+#define MULTIBOOT_TAG_TYPE_EFI64_IH         20
+#define MULTIBOOT_TAG_TYPE_LOAD_BASE_ADDR   21
+
+struct multiboot_tag {
+    uint32_t type;
+    uint32_t size;
+} __attribute__((packed));
+
+struct multiboot_tag_string {
+    uint32_t type;
+    uint32_t size;
+    char string[0];
+} __attribute__((packed));
+
+struct multiboot_tag_module {
+    uint32_t type;
+    uint32_t size;
+    uint32_t mod_start;
+    uint32_t mod_end;
+    char cmdline[0];
+} __attribute__((packed));
+
+struct multiboot_tag_basic_meminfo {
+    uint32_t type;
+    uint32_t size;
+    uint32_t mem_lower;
+    uint32_t mem_upper;
+} __attribute__((packed));
+
+struct multiboot_mmap_entry {
+    uint64_t addr;
+    uint64_t len;
+    uint32_t type;
+    uint32_t zero;
+} __attribute__((packed));
+
+struct multiboot_tag_mmap {
+    uint32_t type;
+    uint32_t size;
+    uint32_t entry_size;
+    uint32_t entry_version;
+    struct multiboot_mmap_entry entries[0];
+} __attribute__((packed));
+
+// Function declarations
+void multiboot2_parse(uint32_t magic, uint32_t mb2_info);
+struct multiboot_tag* multiboot2_find_tag(uint32_t mb2_info, uint32_t type);
+void multiboot2_print_modules(uint32_t mb2_info);
+
+#endif // MULTIBOOT2_H
