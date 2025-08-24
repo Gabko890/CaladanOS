980d8e4d69678b39616a473bfce83fc4604b3fd3
WonTeu <gabriel.szabo2006@gmail.com>
2025-08-24 02:54:15 +0200

    fixed multiboot2_get_memory_regions cause kernel crash

--- kernel/src/ram_manager.c
+++ kernel/src/ram_manager.c
@@ -2,6 +2,8 @@
 #include <multiboot/multiboot2.h>
 #include <stdint.h>
 
+#include <vgaio.h>
+
 uint64_t init_ram_manager(struct mb2_modules_list modules_list, struct mb2_memory_map memory_map) {
     uint64_t first_ram_avialable;
 
@@ -11,7 +13,9 @@ uint64_t init_ram_manager(struct mb2_modules_list modules_list, struct mb2_memor
         extern char _end;
         first_ram_avialable = &_end;
     }
-
+    
+    if (0 == memory_map.count) return 0x00;
+    vga_printf("found %d enties in memory_map\n", memory_map.count);
 
     return first_ram_avialable;
 }
